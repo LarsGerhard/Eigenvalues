12cb876bdef8e809284a358e9fd48d680a9d975c
thaseus <thaseus@uw.edu>
2022-02-03 15:17:17 -0800

    Finished in-class

--- SchrodingerEigenvalues.py
+++ SchrodingerEigenvalues.py
@@ -1,25 +1,27 @@
-from numpy import argsort
+from numpy import argsort, linspace, eye
 from numpy.linalg import eig
-from scipy.constants import hbar, m_e, eV, c
+from matplotlib.pyplot import plot,legend, show
+from scipy.constants import hbar, c, eV, angstrom
 
 # Parameters
-a=m_e # m size of electron
-m= eV / c**2
+a = angstrom # m size of electron
+m = eV / c**2
 
-L = # length of domain -10a<x<10a
-N = 100 # number of grid cells
-dx = # size of grid cell
+L = 20 * a # length of domain -10a<x<10a
+N = 101 # number of grid cells
+dx = L / (N - 1) # size of grid cell
 
 # set the x interval for solving, 10 electron radii each direction: -10a < x < 10a
-x =  # x runs -L/2 to +L/2 by steps dx
+x = linspace((-L / 2), (L / 2), N) # x runs -L/2 to +L/2 by steps dx
 
 # Define the potential well
 # either zero for square well or like above for quadratic well
-Vpot =
+Vpot = 0
 
 # Form the matrix
-Tdiag = # tridiagonal 2nd derivative
-H =
+Tdiag = -2 * eye(N) + 1 * eye(N,N,1) + 1 * eye(N,N,-1)  # tridiagonal 2nd derivative
+M = (1 / L**2) * Tdiag
+H = -(hbar**2 / (2 * m)) * M + Vpot
 
 # Get eigenvalues and eigenvectors
 w,v = eig(H)
@@ -30,7 +32,8 @@ E = w[idx[:3]]
 Psi = v[:,idx[:3]]
 
 # plot lowest 3 energy levels
-from matplotlib.pyplot import plot,xlabel,ylabel,legend,show, figure, subplot
+
 for i in range(3):
     plot(x,Psi[:,i], label='E='+str(round(E[i],1)))
 legend()
+show()
